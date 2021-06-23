75e8c7892c6e6aaa9a10ec4e2d83de2cc85eaec4
Boburjonm <boburjonmirsalimov@mail.ru>
2021-06-23 16:01:12 +0500

    model classes created Pizza and its Category entities

---
+++ WAD.WebApp.7947.DAL/DTO/Category.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WAD.WebApp._7947.DAL.DTO
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+
+        public string CategoryName { get; set; }
+    }
+}
---
+++ WAD.WebApp.7947.DAL/DTO/Pizza.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace WAD.WebApp._7947.DAL.DTO
+{
+    public class Pizza
+    {
+        public int PizzaId { get; set; }
+
+        [Required]
+        [MinLength(4)]
+        public string PizzaName { get; set; }
+
+        [Required]
+        public decimal Price { get; set; }
+
+
+        [Display(Name = "Category")]
+        public int CategoryId { get; set; }
+
+        public Category Category { get; set; }
+
+        [Required]
+        public PizzaSize Size { get; set; }
+
+        [Display(Name = "Pizza Photo")]
+        public byte[] PizzaBinPhoto { get; set; }
+
+        [Display(Name = "Photo")]
+        [DataType(DataType.Upload)]
+        [NotMapped]
+        public IFormFile SteakPhoto { get; set; }
+    }
+    public enum PizzaSize
+    {
+        Small,
+        Medium,
+        Large
+    }
+}
